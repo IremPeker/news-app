672df132071952c96c80da37027775b07893d347
IremPeker <irem.peker@gmx.de>
2019-12-09 12:01:59 +0100

    first commit

---
+++ src/scripts/index.js
@@ -0,0 +1,12 @@
+// The following line makes sure your styles are included in the project. Don't remove this.
+import "../styles/main.scss";
+// Import any additional modules you want to include below \/
+
+// \/ All of your javascript should go here \/
+
+const searchInput = document.querySelector("#news-search");
+searchInput.onkeyup = getValue;
+
+function getValue(e) {
+  console.log(e);
+}
